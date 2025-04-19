02d5e133efb7093b9d02c3d21c7164efaf6f77e8
ruru <ruru.dev07@gmail.com>
2025-04-19 21:51:40 +0530

    refactor: simplify next.config.ts by removing unused experimental features and path imports

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 import bundleAnalyzer from "@next/bundle-analyzer";
-import path from "path";
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
@@ -9,20 +8,12 @@ const withBundleAnalyzer = bundleAnalyzer({
 const nextConfig: NextConfig = {
   transpilePackages: ["@noutify/ui"],
   experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-    },
     useCache: true,
     nodeMiddleware: true,
-    parallelServerBuildTraces: true,
-    webpackBuildWorker: true,
-    nextScriptWorkers: true,
     viewTransition: true,
     reactCompiler: true,
     ppr: true,
   },
-  // TODO: will make a seprate build for the server and client.
-  // cacheMaxMemorySize: 0,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
@@ -39,11 +30,6 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
-  },
-  // outputFileTracingRoot: path.join(__dirname, '../../'),
-  outputFileTracingRoot: path.join(__dirname, "./"),
-  outputFileTracingIncludes: {
-    "*": ["public/**/*", ".next/static/**/*"],
   },
 };
 
